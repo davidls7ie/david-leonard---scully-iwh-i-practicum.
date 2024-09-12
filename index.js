2434d85556869b9da3fad36080a57dfe972f6daa
David Leonard-Scully <dleonardscully@hubspot.com>
2024-09-12 18:30:19 +0100

    Prepping Example for POST

--- index.js
+++ index.js
@@ -2,30 +2,63 @@
 npm list -g 
 */
 
+// globals 
+require('dotenv').config();
+
+// packages
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+
 /* - pass along ? 
-var update_title = 'Update Custom Object Form | Integrating With HubSpot I Practicum.'
+    var update_title = 'Update Custom Object Form | Integrating With HubSpot I Practicum.'
 */
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// Auth Key
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_ACCESS_TOKEN;
+
+// Route 0 - Example code 
+
+app.get('/contacts', async (req, res) => {
+
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
+
+// TODO: ROUTE 1 - 
+// Create a new app.get route for the homepage to call your custom object data. 
+// Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', (req, res) => {
-    res.send('Welcome home! ')
+
+    res.render('index', { 
+        title: 'Home | Integrating With HubSpot I Practicum' ,
+        my_message: 'Welcome home',
+    });
   })
 
+// TODO: ROUTE 2 - 
+// Create a new app.get route for the form to create or update new custom object data. 
+// Send this data along in the next route.
 
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 app.get('/update-cobj', (req, res) => {
     var title = 'Update Custom Object Form | Integrating With HubSpot I Practicum.'
 
@@ -33,21 +66,35 @@ app.get('/update-cobj', (req, res) => {
         title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' ,
         my_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies eros vel dui sagittis convallis. Proin vestibulum sapien sem, et dictum justo ultricies et',
     });
-    // res.send('GET - Update to custom object goes here')
+
   })
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+// TODO: ROUTE 3 - 
+// Create a new app.post route for the custom objects form to create
+// or update your custom object data. 
+// Once executed, redirect the user to the homepage.
 
+// needs async / await, promise to pass data 
+app.post('/update-cobj',  async (req, res) => {
 
-app.post('/update-cobj', (req, res) => {
-
-    res.render('index', { 
-        title: 'Home | Integrating With HubSpot I Practicum' ,
-        my_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies eros vel dui sagittis convallis. Proin vestibulum sapien sem, et dictum justo ultricies et',
-    });
+    const { name, email } = req.body;
     
-  })
+    // Headers Object
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
 
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+  })
+  
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
