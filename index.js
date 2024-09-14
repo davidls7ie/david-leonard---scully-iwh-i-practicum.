0e9af8c0e292893e6f042a99314a1ee0c941cb54
David Leonard-Scully <dleonardscully@hubspot.com>
2024-09-14 15:02:12 +0100

    Printing IDs in the cards and updating the index.js home page route.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 require('dotenv').config();
 
 // Auth Key
-
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_ACCESS_TOKEN;
 
 // Route 0 - Example code  / Works as expected
@@ -25,18 +24,19 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_ACCESS_TOKEN;
 
 
 app.get('/', async (req, res) => {
-
   
-  const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+  const services = 'https://api.hubapi.com/crm/v3/objects/2-34405873';
+  // https://api.hubapi.com/crm/v3/objects/2-34405873
   const headers = {
       Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
       'Content-Type': 'application/json'
   }
 
   try {
-      const resp = await axios.get(contacts, { headers });
+      const resp = await axios.get(services, { headers });
       const data = resp.data.results;
-      res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+      console.log(data)
+      res.render('index', { title: 'Practicum Homepage | HubSpot APIs', data });      
   } catch (error) {
       console.error(error);
   }
@@ -52,21 +52,21 @@ app.get('/update-contact', (req, res) => {
     var title = 'Update Custom Object Form | Integrating With HubSpot I Practicum.'
 
     res.render('updates', { 
-        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' ,
-        my_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies eros vel dui sagittis convallis. Proin vestibulum sapien sem, et dictum justo ultricies et',
+        title: 'Add a Contact | Integrating With HubSpot I Practicum' ,
+        my_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
     });
 
   })
 
-  // app.get('/cobject', (req, res) => {
-  //   res.render('updates', { 
-  //       title: 'List Objects| Integrating With HubSpot I Practicum' ,
-  //       my_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies eros vel dui sagittis convallis. Proin vestibulum sapien sem, et dictum justo ultricies et',
-  //   });
 
-  // })
+  app.get('/update-service', (req, res) => {
+    var title = 'Update Custom Object Form | Integrating With HubSpot I Practicum.'
 
-  https://app.hubspot.com/contacts/<test-account-id>/objects/<custom-object-id>/views/all/list
+    res.render('update-service', { 
+        title: 'Add a Service | Integrating With HubSpot I Practicum' ,
+    });
+
+  })
 
 // TODO: ROUTE 3 - 
 // Create a new app.post route for the custom objects form to create
@@ -74,15 +74,12 @@ app.get('/update-contact', (req, res) => {
 // Once executed, redirect the user to the homepage.
 // needs async / await, promise to pass data from form to CRM
 
-
-
-// https://api.hubapi.com/crm/v3/objects/2-34405873
-
-// 
-
-app.post('/addservice', async (req, res) => {
+app.post('/add-service', async (req, res) => {
     
-  const { name, marketing_service_name, service_description} = req.body;
+  const { name, 
+          marketing_service_name, 
+          service_description
+        } = req.body;
 
   const cobject_data = {
     
@@ -91,17 +88,14 @@ app.post('/addservice', async (req, res) => {
         "marketing_service_name" : req.body.marketing_service_name,
         "service_description" : req.body.service_description
       }
-    
   };
-
   const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     'Content-Type': 'application/json'
   };
 
-  // update the query string to take new values - 19:40 
   try {
-    const response = await axios.post('https://api.hubapi.com/crm/v3/objects/contacts', cobject_data, { headers });
+    const response = await axios.post('https://api.hubapi.com/crm/v3/objects/2-34405873', cobject_data, { headers });
     res.redirect('/');
   } catch (error) {
     console.error('Something went wrong:', error);
